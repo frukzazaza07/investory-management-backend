809faa0f29a52d7192ec372407ae788f231deac1
DESKTOP-UK8MVSK\User <frukza3000@hotmail.com>
2026-07-12 11:47:16 +0700

    save

--- internal/router/router.go
+++ internal/router/router.go
@@ -7,6 +7,7 @@ import (
 	"investory-management-backend/pkg/response"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/swaggo/swag"
 )
 
@@ -14,6 +15,11 @@ func SetupRoutes(app *fiber.App) {
 	app.Get("/health", func(c fiber.Ctx) error {
 		return response.Success(c, "ok", nil)
 	})
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"https://investory-management-backend-production.up.railway.app"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+	}))
 
 	app.Get("/swagger/doc.json", func(c fiber.Ctx) error {
 		doc, err := swag.ReadDoc()
